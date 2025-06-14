fb751412f0f680e7a40efa74dcf761377d6b5620
Alicia Perry <alicia.perry@aquia.us>
2025-06-14 15:17:29 -0500

    adding to deck a deal function that returns two values

--- card_game_cont/deck.go
+++ card_game_cont/deck.go
@@ -27,6 +27,10 @@ func (d deck) print() {
 	}
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:] // we want to return first the slice from the start until the hand size then the handize until the end
+}
+
 // we used a receiver argument, which is what is before the print()
 // by putting what's called a receiver argument in the function
 // A receiver argument essentially allows any variable of type 'deck'
@@ -37,3 +41,8 @@ func (d deck) print() {
 // While we use the word deck to define the type for the variable
 // don't use this or self in go, always refer to the receiver by what it actually is
 // receiver arguments should only be one or two characters that matches the type of the receiver
+
+// To do similar slicing and stepping like in python, you would do something like this:
+// fruits= []string{"Apple", "Banana", "Orange", "Pear"}
+// fmt.Println(fruits[1:3]) // prints [Banana Orange] - this means it's start inclusive, but up to and not including the end index
+// We can also leave out the start or end indexes if we want it to go from the start or until the end
--- card_game_cont/main.go
+++ card_game_cont/main.go
@@ -2,5 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingCards := deal(cards, 5) // deal the first 5 cards from the deck
+
+	hand.print() // print the dealt hand
+	remainingCards.print() // print the remaining cards in the deck
 }
