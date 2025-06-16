f3fb11c9e4b5583c35615a1f68142d7378cecbe1
Alicia Perry <alicia.perry@aquia.us>
2025-06-16 15:56:14 -0500

    making an even or odd thing

---
+++ even_or_odd/main.go
@@ -0,0 +1,10 @@
+package main
+
+// we're going to make a main function
+// that creates a slice of ints from 0 to 10
+// Iterate through the slice and for each element see
+// if the element is even or odd
+
+func main() {
+	isEvenOrOdd()
+}
