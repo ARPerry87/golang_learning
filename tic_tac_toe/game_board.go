019dc2d0dba454fa1f09fff957520d7c0e8e5879
Alicia Perry <alicia.perry@aquia.us>
2025-06-18 14:01:07 -0500

    refactor

--- tic_tac_toe/game_board.go
+++ tic_tac_toe/game_board.go
@@ -1,44 +1,50 @@
 package main
 
+const (
+	PlayerX = "X"
+	PlayerO = "O"
+	Empty   = " "
+)
+
 // This will represent the game board
 type Board struct {
 	Cells [3][3]string
 }
 
 // create a board with fully filled in cells with o's and x's
-
 func filledBoard() Board {
 	return Board{
 		Cells: [3][3]string{
-			{"X", "O", "X"},
-			{"O", "X", "O"},
-			{"O", "X", "X"},
+			{PlayerX, PlayerO, PlayerX},
+			{PlayerO, PlayerX, PlayerO},
+			{PlayerO, PlayerX, PlayerX},
 		},
 	}
 }
 
 func (b *Board) checkWin() string {
-	// Check rows
-	for i := range 3 {
-		if b.Cells[i][0] == b.Cells[i][1] && b.Cells[i][1] == b.Cells[i][2] && b.Cells[i][0] != " " {
-			return b.Cells[i][0]
+	winPatterns := [8][3][2]int{
+		// Rows
+		{{0, 0}, {0, 1}, {0, 2}},
+		{{1, 0}, {1, 1}, {1, 2}},
+		{{2, 0}, {2, 1}, {2, 2}},
+		// Columns
+		{{0, 0}, {1, 0}, {2, 0}},
+		{{0, 1}, {1, 1}, {2, 1}},
+		{{0, 2}, {1, 2}, {2, 2}},
+		// Diagonals
+		{{0, 0}, {1, 1}, {2, 2}},
+		{{0, 2}, {1, 1}, {2, 0}},
+	}
+
+	for _, pattern := range winPatterns {
+		a, b1, c := pattern[0], pattern[1], pattern[2]
+		symbol := b.Cells[a[0]][a[1]]
+		if symbol != Empty &&
+			symbol == b.Cells[b1[0]][b1[1]] &&
+			symbol == b.Cells[c[0]][c[1]] {
+			return symbol
 		}
 	}
-
-	// Check columns
-	for j := range 3 {
-		if b.Cells[0][j] == b.Cells[1][j] && b.Cells[1][j] == b.Cells[2][j] && b.Cells[0][j] != " " {
-			return b.Cells[0][j]
-		}
-	}
-
-	// Check diagonals
-	if b.Cells[0][0] == b.Cells[1][1] && b.Cells[1][1] == b.Cells[2][2] && b.Cells[0][0] != " " {
-		return b.Cells[0][0]
-	}
-	if b.Cells[0][2] == b.Cells[1][1] && b.Cells[1][1] == b.Cells[2][0] && b.Cells[0][2] != " " {
-		return b.Cells[0][2]
-	}
-
 	return ""
 }
