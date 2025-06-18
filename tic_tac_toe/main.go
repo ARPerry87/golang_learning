3ef0042471ef0edc43586bb15a210e27cf9970e7
Alicia Perry <alicia.perry@aquia.us>
2025-06-18 14:02:23 -0500

    further refactor

--- tic_tac_toe/main.go
+++ tic_tac_toe/main.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    board := filledBoard()
-    fmt.Println("Filled Board:")
-    for _, row := range board.Cells {
-        fmt.Println(row)
-    }
+	board := filledBoard()
 
-    winner := board.checkWin()
-    if winner != "" {
-        fmt.Printf("Winner: %s\n", winner)
-    } else {
-        fmt.Println("No winner.")
-    }
+	fmt.Println("Board:")
+	for _, row := range board.Cells {
+		fmt.Println(row)
+	}
+
+	winner := board.checkWin()
+	if winner != "" {
+		fmt.Printf("Winner: %s\n", winner)
+	} else {
+		fmt.Println("No winner.")
+	}
 }
